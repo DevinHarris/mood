424ed4cb5581d3ed73bca5bf1d30f71c3a7e0405
Devonta Harris <devontah94@gmail.com>
2022-03-16 18:25:02 -0400

    cors fix

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const moodRoutes = require('./routes/moods');
 
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.options('*', cors());
 app.use(express.json());
 app.use(moodRoutes);
