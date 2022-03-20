d2908a96b1d9c23b2e8f0c6dc1bff50dcc4cb64e
Devonta Harris <devontah94@gmail.com>
2022-03-19 20:00:40 -0400

    updates

--- src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: 'https://m-ood.herokuapp.com/',
-    
-})
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import moodSlice from "../components/moodSlice";
-
-export default configureStore({
-    reducer: {
-        mood: moodSlice
-    }
-})
